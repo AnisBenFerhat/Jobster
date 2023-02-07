ca3460328bd2773829b5ef2fef82597936389693
AnisBenFerhat <anisbenferhat@gmail.com>
2023-02-07 16:08:16 +0100

    Register Page - Setting Form Structure

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,6 @@
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/Landing';
-import { Logo } from '../components/Index';
+import { Logo } from '../components';
 import { Link } from 'react-router-dom';
 
 const Landing = () => {
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,40 @@
+import { useState, useEffect } from 'react';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  const handleChange = (event) => {
+    console.log(event.target);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  };
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/*Name Fielde*/}
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            Name
+          </label>
+          <input type='text' name='name' value={values.name} onChange={handleChange} className='form-input' />
+        </div>
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
